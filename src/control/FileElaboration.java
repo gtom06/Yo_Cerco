54c54ea742947d691b37e55145f1156e48461bd0
gtom06 <56637690+gtom06@users.noreply.github.com>
2022-09-11 20:18:29 +0200

    mod

--- src/control/FileElaboration.java
+++ src/control/FileElaboration.java
@@ -9,51 +9,80 @@ public class FileElaboration {
     }
 
     public static void writeOnFile(String filepath, String stringToWrite) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter(filepath));
+        BufferedWriter writer = null;
         try {
+            writer = new BufferedWriter(new FileWriter(filepath));
             writer.write(stringToWrite);
         } catch (Exception e){
             e.printStackTrace();
+        } finally{
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        writer.close();
     }
 
     public static ArrayList<String> fileLinesToArrayList(String filepath) throws IOException {
         ArrayList<String> lines = new ArrayList<>();
-        BufferedReader br = new BufferedReader(new FileReader(filepath));
-        String line;
-        while ((line = br.readLine()) != null) {
-            lines.add(line);
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null){
+                    br.close();
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
         }
-        br.close();
         return lines;
     }
 
     public static String fileToString(String filepath) {
         String output = "";
+        BufferedReader bufferedReader = null;
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(filepath));
+            bufferedReader = new BufferedReader(new FileReader(filepath));
             output = bufferedReader.readLine();
-            bufferedReader.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
         finally {
-            return output;
+            try {
+                if (bufferedReader != null){
+                    bufferedReader.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+        return output;
     }
 
     public static boolean copyAndReplaceFile(File file, String pathTo) {
         try {
             File fileToDelete = new File(pathTo);
             if (fileToDelete != null) {
-                file.renameTo(new File(pathTo));
+                if (!file.renameTo(new File(pathTo))){
+                    return false;
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
             return false;
-        } finally {
-            return true;
         }
+        return true;
     }
 }
