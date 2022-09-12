b6318d347c827cb0cc347e62c1b86b6f66f2ffbf
gtom06 <56637690+gtom06@users.noreply.github.com>
2022-09-12 15:34:09 +0200

    bug fix

--- src/view/view1/CartAndPayment.java
+++ src/view/view1/CartAndPayment.java
@@ -158,25 +158,25 @@ public class CartAndPayment {
         yyTextField.setVisible(true);
         cvvTextField.setVisible(true);
         slashText.setVisible(true);
-        if (user.getName() != null || user.getName() != "") {
+        if (user.getName() != null || !user.getName().equals("")) {
             nameTextField.setText(user.getName());
         }
-        if (user.getSurname() != null || user.getUsername() != "") {
+        if (user.getSurname() != null || !user.getUsername().equals("")) {
             surnameTextField.setText(user.getSurname());
         }
-        if (((Buyer) user).getPhone() != null || ((Buyer) user).getPhone() != "") {
+        if (((Buyer) user).getPhone() != null || !((Buyer) user).getPhone().equals("")) {
             phoneNumberTextField.setText(((Buyer) user).getPhone());
         }
-        if (((Buyer) user).getBillingStreet()!= null || (((Buyer) user).getBillingStreet()) != "") {
+        if (((Buyer) user).getBillingStreet()!= null || !(((Buyer) user).getBillingStreet()).equals("")) {
             billingStreetTextField.setText(((Buyer) user).getBillingStreet());
         }
-        if (((Buyer) user).getPhone() != null || ((Buyer) user).getPhone() != "") {
+        if (((Buyer) user).getPhone() != null || !((Buyer) user).getPhone().equals("")) {
             billingCityTextField.setText(((Buyer) user).getBillingCity());
         }
-        if (((Buyer) user).getBillingCountry() != null || ((Buyer) user).getBillingCountry() != "") {
+        if (((Buyer) user).getBillingCountry() != null || !((Buyer) user).getBillingCountry().equals("")) {
             billingCountryTextField.setText(((Buyer) user).getBillingCountry());
         }
-        if (((Buyer) user).getBillingZip() != null || ((Buyer) user).getBillingZip() != "") {
+        if (((Buyer) user).getBillingZip() != null || !((Buyer) user).getBillingZip().equals("")) {
             billingZipTextField.setText(((Buyer) user).getBillingZip());
         }
     }
