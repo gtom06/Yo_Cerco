61affbca4a7f4955370cee473afb163721803eb2
gtom06 <56637690+gtom06@users.noreply.github.com>
2022-09-20 23:16:59 +0200

    add PaymentHandler

--- src/control/PaymentHandler.java
+++ src/control/PaymentHandler.java
@@ -1,8 +1,13 @@
 package control;
 
 import constants.Constants;
+import constants.ConstantsExceptions;
 
 public class PaymentHandler {
+    private PaymentHandler(){
+        throw new IllegalStateException(ConstantsExceptions.UTILITY_CLASS_INFO);
+    }
+
     public static boolean validateParams(String payment, String cardNumber, String mm, String yy, String cvv) {
         if (payment.equals(Constants.BY_CARD)){
             if (cardNumber.length() < 16 && mm.length() == 0 && yy.length() == 0 && cvv.length() < 3) {
